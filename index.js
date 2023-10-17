61bd3947e98c9eb116dcdd50f3eabdee035df7d9
joshua_hill <joshuah923@gmail.comc:/Users/joshu/OneDrive/Desktop/hacker/CSS-The-Hacker-Times-Part-1/CSS-The-Hacker-Times-Part-1/index.html>
2023-10-17 15:43:14 -0700

    added screen and shadow

--- index.js
+++ index.js
@@ -8,13 +8,15 @@ let speed = 7;
 function drawGame(){
 clearScreen();
  setTimeout(drawGame,1000/ speed);
- 
+}
+
+
 function clearScreen(){
 context.fillStlyle = 'black';
 context.fillRect(0,0,canvas.clientWidth,canvas.height);
 }
 
-}
+
 
 
 drawGame();
