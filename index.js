b36beef3f1024f02a58195b55eeafa8d41fc41aa
joshua_hill <joshuah923@gmail.comc:/Users/joshu/OneDrive/Desktop/hacker/CSS-The-Hacker-Times-Part-1/CSS-The-Hacker-Times-Part-1/index.html>
2023-10-18 19:04:36 -0700

    added snake head

--- index.js
+++ index.js
@@ -4,19 +4,35 @@ const canvas= document.getElementById('boarder');
 const context = canvas.getContext('2d');
 // speed of game is by seconds
 let speed = 7;
-// game loop updates screen
+
+let titleCount = 20;
+let tileSize = canvas.width/ titleCount -9;
+let headX = 20;
+let headY = 20;
+
+// speed of the snake
+let xvelocity=0;
+let yvelocity=0;
+
+
+
+// game loop updates screen 
 function drawGame(){
 clearScreen();
- setTimeout(drawGame,1000/ speed);
+drawSnake();
+setTimeout(drawGame,1000/ speed);
 }
 
 
 function clearScreen(){
 context.fillStlyle = 'black';
-context.fillRect(0,0,canvas.clientWidth,canvas.height);
+context.fillRect(0,0,canvas.Width,canvas.height);
 }
-
-
+// adds snake color and shape
+function drawSnake(){
+    context.fillStyle = "red" 
+    context.fillRect(headX* titleCount, headY* titleCount, tileSize, tileSize )
+}
 
 
 drawGame();
